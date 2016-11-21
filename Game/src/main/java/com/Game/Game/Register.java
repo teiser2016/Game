09553255cfd080aa9a0e1c2a21ac59ae7f44dca8
Dimisg <dimisg93@gmail.com>
2016-11-21 16:24:41 +0200

    simplirosi kodika

--- Game/src/main/java/com/Game/Game/Register.java
+++ Game/src/main/java/com/Game/Game/Register.java
@@ -1,9 +1,21 @@
 package com.Game.Game;
 
-import android.support.v7.app.AppCompatActivity;
+
+
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Register extends AppCompatActivity {
 
@@ -20,4 +32,39 @@ public class Register extends AppCompatActivity {
         final Button bRegister = (Button) findViewById(R.id.bRegister);
     }
 
+    bRegister.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            final String username = etUsername.getText().toString();
+            final String mail = etMail.getText().toString();
+            final String password = etPassword.getText().toString();
+
+            Response.Listener<String> responseListener = new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        boolean success = jsonResponse.getBoolean("success");
+                        if (success) {
+                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            RegisterActivity.this.startActivity(intent);
+                        } else {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                            builder.setMessage("Register Failed")
+                                    .setNegativeButton("Retry", null)
+                                    .create()
+                                    .show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            RegisterRequest registerRequest = new RegisterRequest(username, password, mail, responseListener);
+            RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+            queue.add(registerRequest);
+        }
+    })
+}
 }
