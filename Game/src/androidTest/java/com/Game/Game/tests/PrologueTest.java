dad26688d9850af4d2c0f264706de165215ade53
Grigoris Paschalidis <gregpasch8@gmail.com>
2016-12-17 18:18:38 +0200

    Create PrologueTest.java

---
+++ Game/src/androidTest/java/com/Game/Game/tests/PrologueTest.java
@@ -0,0 +1,27 @@
+package com.Game.Game.tests;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Γρηγόρης on 17/12/2016.
+ */
+
+public class PrologueTest extends TestCase
+{
+    public void setUp(){
+        super.setUp();
+    }
+
+
+    public void onCreate{
+    String Prologue = Gregory;
+    String PrologueContinue = GregoryContinue;
+}
+    public void onClick{
+    
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
