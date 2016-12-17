bf49961fb3af4f0637953835914c777afef4a81d
Mitsakos46 <mitsakos994@gmail.com>
2016-12-17 18:03:16 +0200

    Create LoginActivityTest.java

---
+++ Game/src/androidTest/java/com/Game/Game/tests/LoginActivityTest.java
@@ -0,0 +1,24 @@
+package com.Game.Game.tests;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Δημήτρης on 17/12/2016.
+ */
+
+public class LoginActivityTest extends TestCase
+{
+public void setUp(){
+    super.setUp();
+}
+
+
+public void onClick() {
+    String username = Mitsakos;
+    String password = 123456arfz;
+}
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
