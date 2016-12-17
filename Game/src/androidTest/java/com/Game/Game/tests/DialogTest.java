532d7c66d084377e78819ce71b6a031f270b28a0
Grigoris Paschalidis <gregpasch8@gmail.com>
2016-12-17 18:24:00 +0200

    Create DialogTest.java

---
+++ Game/src/androidTest/java/com/Game/Game/tests/DialogTest.java
@@ -0,0 +1,27 @@
+package com.Game.Game.tests;
+
+import junit.framework.TestCase;
+
+
+public class DialogTest extends TestCase
+{
+    public void setUp(){
+        super.setUp();
+    }
+
+
+    public void onCreate{
+    String Prologue = Gr;
+    
+}
+    public void onClick{
+
+}
+    public void setText{
+    Text test1 = My name is Lantzos;
+}
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
