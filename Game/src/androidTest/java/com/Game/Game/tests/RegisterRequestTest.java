97dbee9b04d2d31cd6b2c3953f8a51716d73506b
Dimisg <dimisg93@gmail.com>
2016-12-17 17:58:13 +0200

    RegisterRequest Test

---
+++ Game/src/androidTest/java/com/Game/Game/tests/RegisterRequestTest.java
@@ -0,0 +1,30 @@
+package com.Game.Game.tests;
+
+import junit.framework.TestCase;
+
+
+public class RegisterRequestTest  extends TestCase {
+
+    public void setup() throws Exception {
+        super.setUp();
+    }
+
+
+
+    public RegisterRequestTest() throws Exception{
+        String username = "grigoris";
+        String password = "1236485";
+        String mail = "gri12@gmail.com";
+    }
+
+    public RegisterRequestTest() {
+        String username = "grigoris";
+        String password = "1236485";
+        String mail = "gri12@gmail.com";
+    }
+
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
