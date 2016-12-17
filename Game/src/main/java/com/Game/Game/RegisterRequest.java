5de11fc523a01cc2db1e9a826e9693aa1908bc66
Dimisg <dimisg93@gmail.com>
2016-12-17 16:49:52 +0200

    link register

--- Game/src/main/java/com/Game/Game/RegisterRequest.java
+++ Game/src/main/java/com/Game/Game/RegisterRequest.java
@@ -3,12 +3,11 @@ package com.Game.Game;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
 public class RegisterRequest extends StringRequest {
-    private static final String REGISTER_REQUEST_URL = "APOSTILI TO DIKO MAS DOMAIN APO TO WEBHOST 8ELW EDW";
+    private static final String REGISTER_REQUEST_URL = "http://teiser2016.000webhostapp.com/register.php";
     private Map<String, String> params;
 
     public RegisterRequest(String username, String password, String mail, Response.Listener<String> listener) {
