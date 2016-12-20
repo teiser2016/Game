d459d143cf07f4795e4b9776144339c6728e76d4
Asks <kakos5002@yahoo.com>
2016-12-21 01:35:25 +0200

    CharToUser Test

---
+++ Game/src/androidTest/java/com/Game/Game/tests/CharToUserTest.java
@@ -0,0 +1,36 @@
+package com.Game.Game.tests;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import com.Game.Game.CharToUser;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Αποστόλης on 21/12/2016.
+ */
+
+public class CharToUserTest extends TestCase
+{
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+    }
+
+    @SmallTest
+    public CharToUser()
+    {
+        String Cd = "1";
+        String usernmae = "Apos"" ;
+    }
+
+    
+
+    @Override
+    protected void tearDown() throws Exception
+    {
+        super.tearDown();
+    }
+
+}
